0002fb0ae72ed6c500c1348a74a535aab12d0923
Clay Richard <62192357+alanclayrichard@users.noreply.github.com>
2022-04-17 23:03:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='math_tools',
-    version='0.0.7-beta.1',
+    name='chemE_tools',
+    version='0.0.7-beta.2',
     author='Clay Richard',
     author_email='alanclayrichard@gmail.com',
     description='Testing installation of Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/alanclayrichard/math',
+    url='https://github.com/alanclayrichard/chemE',
     license='MIT',
-    packages=['math_tools'],
+    packages=['chemE_tools'],
     install_requires=['requests'],
 )
