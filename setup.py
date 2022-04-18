5c0fa3f81f49ae8f639c814bb082faa0bf5b3e68
Clay Richard <62192357+alanclayrichard@users.noreply.github.com>
2022-04-17 22:48:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='math_tools',
-    version='0.0.6',
+    version='0.0.7',
     author='Clay Richard',
     author_email='alanclayrichard@gmail.com',
     description='Testing installation of Package',
