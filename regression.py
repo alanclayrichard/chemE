a281a641bedb37236438171964e092c1c3ae44a6
Clay Richard <alanclayrichard@gmail.com>
2022-04-17 19:59:18 -0500

    -updates

--- regression.py
+++ regression.py
@@ -1,6 +1,9 @@
 import numpy as np
 import numpy.typing as npt
 from matplotlib import pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 class Regression:
     def get_mult_linreg(x_train: npt.NDArray, y_train: npt.NDArray, order: int, x_test: npt.NDArray = False) -> npt.NDArray:
@@ -26,15 +29,21 @@ class Regression:
                 mult_linreg_mdl = Betas[0]
                 for j in range(1,order+1):
                     mult_linreg_mdl = mult_linreg_mdl + Betas[j]*(x_test**j)
-            return mult_linreg_mdl
+            return mult_linreg_mdl, Betas
 
-    def get_r2(x_train: npt.NDArray, y_train: npt.NDArray) -> float:
-        yhat = np.mean(y_train)
-        ybar = Regression.get_mult_linreg(x_train,y_train,1)
-        rss = np.sum((y_train-ybar)**2)
-        tss = np.sum((y_train-yhat)**2)
-        r_sq = 1 - rss/tss
-        return r_sq
+    def get_r2(x: npt.NDArray, y: npt.NDArray) -> float:
+        yhat = np.mean(y)
+        pred,betas = Regression.get_mult_linreg(x,y,1)
+        RSS = np.sum(np.square(y-pred))
+        TSS = np.sum(np.square(y-yhat))
+        return 1 - RSS/TSS
+
+    def get_adj_r2(x: npt.NDArray, y: npt.NDArray, n, d) -> float:
+        yhat,betas = Regression.get_mult_linreg(x,y,1)
+        ybar = np.mean(y)
+        RSS = np.sum(np.square((y - yhat)))
+        TSS = np.sum(np.square((y - ybar)))
+        return 1-((RSS/(n-d-1))/(TSS/(n-1)))
 
     def checknan(array: npt.NDArray,name: str = "your stinky data") -> bool:
         k = len(np.shape(array))
