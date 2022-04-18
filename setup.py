74201bc196305480edd54bf2e5995ab27e1e5464
Clay Richard <62192357+alanclayrichard@users.noreply.github.com>
2022-04-17 21:34:01 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='math_tools',
+    version='0.0.1_beta',
+    author='Clay Richard',
+    author_email='alanclayrichard@gmail.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/alanclayrichard/math',
+#     project_urls = {
+#         "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+#     },
+    license='MIT',
+    packages=['math_tools'],
+    install_requires=['requests'],
+)
