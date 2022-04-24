df9036ada15ac53d8bce3189f198ac13cf22aaf7
Clay Richard <alanclayrichard@gmail.com>
2022-04-24 00:00:20 -0500

    fixed rk45 to work now // example added

--- chemE_tools/examples.py
+++ chemE_tools/examples.py
@@ -4,6 +4,9 @@ import math_funcs
 import math
 
 rk4 = math_funcs.ODE.rk4
+rk45 = math_funcs.ODE.rk45
+
+## ode's governing predator prey btw rabbits and fox
 def rabbits(t,x):
     # define variables
     a = x[0]
@@ -27,16 +30,42 @@ def rabbits(t,x):
     dxdt = np.array([dx1dt, dx2dt])
     return dxdt
 
-t1,y1 = rk4(rabbits,0,500,[500,200],.01)
-plt.plot(t1,y1)
+## ode's governing a Lorenz system
+def lorenz(t,x):
+    # define variables
+    a = x[0]
+    b = x[1]
+    c = x[2]
+
+    # set parameters
+    sigma = 10
+    rho = 28
+    beta = 8/3
+
+    #gather odes
+    dadt = sigma*(b - a)
+    dbdt = a*(rho - c) - b
+    dcdt = a*b - beta*c
+
+    # return combined variable ode
+    dxdt = np.array([dadt, dbdt, dcdt])
+    return dxdt
+
+# # how to use the 4th order RK ode solver
+# t1,y1 = rk4(rabbits,0,500,[500,200],.01)
+# plt.plot(t1,y1,'red')
+# plt.show()
+
+# how to use the RKF45 ode solver 
+t2,y2 = rk45(lorenz,0,35,[0,2,20],.01)
+plt.plot(y2[:,0],y2[:,2])
 plt.show()
 
-trapezoid = math_funcs.Integration.trapezoid
-
-def f(x):
-    return np.exp(-x**2)
-
-z = 1
-area = trapezoid(f,0,z,.001)
-print((2/np.sqrt(np.pi))*area)
-print(math.erf(z))
+# # how to use the definite integral function to integrate the error function
+# trapezoid = math_funcs.Integration.trapezoid
+# def f(x):
+#     return np.exp(-x**2)
+# z = 1
+# area = trapezoid(f,0,z,.001)
+# print((2/np.sqrt(np.pi))*area)
+# print(math.erf(z))
--- chemE_tools/math_funcs.py
+++ chemE_tools/math_funcs.py
@@ -82,7 +82,7 @@ class ODE:
         t[0] = tspan[0]
         for j in range(0,num_eqn):
             y[0,j] = ic[j]
-        for i in range(0,n-1):
+        for i in tqdm(range(0,n-1)):
             k1 = f(t[i],y[i,:])
             k2 = f(t[i]+h/2,y[i,:]+h*k1/2)
             k3 = f(t[i]+h/2,y[i,:]+h*k2/2)
@@ -91,58 +91,51 @@ class ODE:
             t[i+1] = t[i]+h
         return t,y
 
-    def rk45(f,t0,tf,ic,step_size): # UNDER CONSTRUCTION
+    def rk45(f,t0,tf,ic,step_size):
         ic = np.array([ic])
-
         fourth_approx = np.array([])
         fifth_approx = np.array([])
-
         tspan = np.array([t0, tf])
-
         h = np.array([])
         t = np.array([])
-        y = np.array([[0,0]])
-
-        h = np.append(h,step_size)
+        y = np.array(ic)
+        h = step_size
         t = np.append(t,tspan[0])
-        y = np.append(y,ic,axis=0)
-
         tol = .00001
         i = 0
+        while t[-1]<tf:
+            k1 = h*f(t[i], y[i])
+            k2 = h*f(t[i] + h/4, y[i] + k1/4)
+            k3 = h*f(t[i] + 3*h/8, y[i] + 3*k1/32 + 9*k2/32)
+            k4 = h*f(t[i] + 12*h/13, y[i] + 1932*k1/2197 - 7200*k2/2197 + 7296*k3/2197)
+            k5 = h*f(t[i] + h, y[i] + 439*k1/216 - 8*k2 + 3680*k3/513 - 845*k4/4104)
+            k6 = h*f(t[i] + h/2, y[i] - 8*k1/27 + 2*k2 - 3544*k3/2565 + 1859*k4/4104 - 11*k5/40)
+            fourth_approx = y[i] + 25*k1/216 + 1408*k3/2565 + 2197*k4/4104 - k5/5
+            fifth_approx = y[i] + 16*k1/135 + 6656*k3/12825 + 28561*k4/56430 - 9*k5/50 + 2*k6/55
 
-        while t[-1] < tspan[1]:
-            flag = False
-            current_step = h[i]
-            while flag == False:
-                k1 = current_step*f(t[i],y[i+1])
-                k2 = current_step*f(t[i]+current_step/4,y[i+1]+k1/4)
-                k3 = current_step*f(t[i]+3*current_step/8,y[i+1]+3*k1/32+9*k2/32)
-                k4 = current_step*f(t[i]+12*current_step/13,y[i+1]+1932*k1/2197-7000*k2/2197+7296*k3/2197)
-                k5 = current_step*f(t[i]+current_step,y[i+1]+439*k1/216-8*k2+3680*k3/513-845*k4/4104)
-                k6 = current_step*f(t[i]+current_step/2,y[i+1]-8*k1/27+2*k2-3544*k3/2565+1859*k4/4104-11*k5/40)
-                fourth_approx = y[i+1] + 25*k1/216+1408*k3/2565+2197*k4/4101-k5/5
-                fifth_approx = y[i+1] + 16*k1/135+6656*k3/12825+28561*k4/56430-9*k5/50+2*k6/55
+            R = np.abs(np.array(fifth_approx)-np.array(fourth_approx))/h
+            R = np.amax(R)
+            delta = (tol/(2*R))**(1/4)
 
-                R = 1/np.array(h[i])*np.abs(np.array(fifth_approx)-np.array(fourth_approx))
-                delta = (tol/(2*R))**(1/4)
-                print(R)
-                if R.all() <= tol:
-                    y = np.append(y,fourth_approx,axis=0)
-                    # print(h)
-                    h = np.append(h,delta*h[i])
-                    # print(h)
-                    t = np.append(t,(t[i]+h[i+1]))
-                    flag = True
+            if R<=tol:
+                t = np.append(t, t[-1] + h)
+                y = np.append(y, np.array([fourth_approx]), axis=0)
+                if delta < 0.1:
+                    h = 0.1*h
+                elif delta > 4:
+                    h = 4*h
                 else:
-                    current_step = delta*h[i]
-                    flag = False
-            i += 1
+                    h = delta*h
+                i+=1
+            else:
+                h = delta*h
+        print("steps:",i+1)
         return t,y
 
 class Integration:
     def trapezoid(function,lower_limit,upper_limit,step_size=.01):
         area = np.zeros(1)
-        for i in np.arange(lower_limit,upper_limit,step_size):
+        for i in tqdm(np.arange(lower_limit,upper_limit,step_size)):
             area = np.append(area,(step_size)*((function(i)+function(i+step_size))/2))
         area = sum(area)
         return area
